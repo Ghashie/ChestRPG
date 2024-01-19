0f1cc6ced6ca8047eef1ba91cac15d6cf902d9f3
Thiago Viannay <thiagoviannay24@gmail.com>
2024-01-19 16:59:49 -0300

    Início do modelo PDO

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitd751713988987e9331980363e24189ce::getLoader();
+return ComposerAutoloaderInit72f12453a35a612dd9af060b3b03391e::getLoader();
