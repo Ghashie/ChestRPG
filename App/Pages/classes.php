dc0a5c85534a7348469551cf1e9ba4f221d48643
Patrick Andrade <128238530+PatrickAndradeSC@users.noreply.github.com>
2024-01-24 15:39:55 -0300

    pagina de classes pronta

---
+++ App/Pages/classes.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../../css/classes.css">
+</head>
+
+<body>
+  <header class="header">
+    <div class="logo">
+      <a href="index.html"><img src="../../img/logo.png" alt="" class="logo-img"></a>
+    </div>
+    <div class="container">
+      <button class="btn">HOME</button>
+      <button class="btn">SOBRE</button>
+      <div class="dropdown">
+        <button class="dropdown-button">MESAS </button>
+        <div class="dropdown-content">
+          <a href="#">Criar mesa</a>
+          <a href="#">Entrar em mesa existente</a>
+        </div>
+      </div>
+      <button class="btn">ATUALIZAÇÕES</button>
+    </div>
+    <div class="login">LOgin</div>
+  </header>
+
+
+
+  <main class="page-content">
+
+
+    
+    <div class="card">
+      <div class="content">
+        <h2 class="title">Guerreiro e Cavaleiro</h2>
+        <p class="copy">Começando pela classe mais popular de qualquer RPG, os Guerreiros ou Cavaleiros são, geralmente,
+          a primeira escolha a ser apresentada nos jogos. Essa classe é a principal responsável por desempenhar o papel
+          de tank devido a alta resistência. Em diversos jogos, inclusive, são as únicas classes capazes de equipar
+          escudos.</p>
+
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">Arqueiro, Caçador e Ranger</h2>
+        <p class="copy">Arqueiros, caçadores e rangers são classes populares em jogos, conhecidas por atacar à distância
+          com arcos e flechas. Ágeis e precisos, oferecem facilidade de jogabilidade, sendo escolhas acessíveis para
+          iniciantes. Além do dano à distância, possuem habilidades furtivas, de rastreamento e táticas, contribuindo
+          significativamente para grupos em estratégias de equipe.</p>
+
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">Mago, Bruxo e Elementalista</h2>
+        <p class="copy">Indispensáveis em qualquer RPG, os Magos, Bruxos ou Elementalistas são capazes de controlar os
+          elementos para usá-los de forma ofensiva ou defensiva. Assim como Arqueiros, sempre atacam de longe e evitam
+          estar perto do perigo. Costumam ser indispensáveis em grupos pela capacidade insana de dano, mas também
+          possuem alta dependência por outras classes que segurem o tranco para eles.</p>
+
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">Assassino, Ladino, Ladrão e Ninja</h2>
+        <p class="copy">Assassinos, Ladinos, Ladrões e Ninjas, classes correlatas em jogos, destacam-se por suas
+          habilidades únicas de furtividade e capacidade de causar dano instantâneo. Apesar da resistência limitada,
+          podem assumir o papel de "tank" em certas situações, especializando-se em eliminar alvos frágeis, como
+          Arqueiros e Magos, com uma jogabilidade baseada em combos.</p>
+
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">Clérigo, Sacerdote e Curandeiro</h2>
+        <p class="copy">Clérigos, Sacerdotes e Curandeiros, por vezes evoluções da mesma classe em alguns jogos, são
+          notáveis como classes de suporte. Suas habilidades são direcionadas ao grupo, incluindo curas, buffs, remoção
+          de debuffs e ressurreições. Embora possam ter ferramentas para jogar sozinhos, a progressão mais eficiente até
+          o nível máximo geralmente ocorre através da experiência compartilhada em grupos.</p>
+
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">Paladino e Templário</h2>
+        <p class="copy">Paladinos e Templários, variações de Guerreiros e Cavaleiros, incorporam magias, buffs, curas e
+          habilidades de suporte em seu gameplay. Em alguns jogos, podem ser evoluções da mesma classe. Seu principal
+          foco é a capacidade de "tankar", com alta resistência e habilidades de cura. Além disso, oferecem suporte
+          diversificado, como compartilhamento de dano e escudos de luz para absorção de dano, diferenciando-se dos
+          Sacerdotes.</p>
+
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">Bárbaro e Berserker</h2>
+        <p class="copy">Bárbaros e Berserkers, variações ofensivas dos Guerreiros, enfrentam o combate de perto com
+          menos foco na defesa. Suas habilidades priorizam auto-buffs para ampliar dano e velocidade de ataque, às vezes
+          com sacrifícios de vida para maior letalidade.</p>
+
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">Feiticeiro, Invocador e Necromante</h2>
+        <p class="copy">Feiticeiros, Invocadores ou Necromantes, variantes de Magos e Bruxos, usam magias sombrias, como
+          roubos de vida e mana, invocações, transformações em demônios, debuffs e maldições causadoras de dano ao longo
+          do tempo. Tendem a depender mais de habilidades de auto-cura em comparação com Magos.
+        </p>
+
+      </div>
+    </div>
+  </main>
+
+  <footer class="footer">
+    <section class="first-footer">
+      <div class="text-first-footer">
+        <h1>SOBRE O CHEST RPG</h1>
+      </div>
+      <div class="text-first-footer">
+        <h1>BAIXE NOSSO APLICATIVO</h1>
+      </div>
+      <div class="text-first-footer">
+        <h1>DEIXE O SEU FEEDBACK</h1>
+      </div>
+      <div class="text-first-footer">
+        <h1>SUPORTE</h1>
+      </div>
+
+    </section>
+
+    <section class="second-footer">
+      <div class="image-second-footer">
+        <div class="image-footer">
+          <img src="../..//img/face.svg" alt="">
+        </div>
+        <div class="image-footer">
+          <img src="../..//img/insta.svg" alt="">
+        </div>
+        <div class="image-footer">
+          <img src="../..//img/twitter.svg" alt="">
+        </div>
+        <div class="image-footer">
+          <img src="../..//img/discord.svg" alt="">
+        </div>
+      </div>
+
+      <div class="logo-footer">
+        <a href="index.html"><img src="../..//img/CHEST_RPG__1_-removebg-preview.png" alt="" class="logo-img-footer"></a>
+      </div>
+      <div class="text-footer">
+        <p>© 2022 CHEST RPG. Todos os direitos reservados.</p>
+      </div>
+    </section>
+  </footer>
+
+
+
+
+
+</body>
+
+</html>
